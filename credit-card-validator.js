5122b8a322adeb3fbdd22945901f8656f71d1e4d
Michael Ryan Dempsey <bluestealth@bluestealth.pw>
2015-03-31 15:52:16 -0700

    Add license annotation

--- credit-card-validator.js
+++ credit-card-validator.js
@@ -1,5 +1,5 @@
-/*
- * Copyright (C) 2015 Michael Dempsey
+/* 
+ * @license Copyright (C) 2015 Michael Dempsey
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
